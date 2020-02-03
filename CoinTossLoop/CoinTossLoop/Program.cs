f7d169968b528b9719f990e455df6b89b480db9c
Matt Boensch <Matthew.D.Boensch-1@ou.edu>
2020-02-03 12:40:01 -0600

    Attempt at loop for coin toss

---
+++ CoinTossLoop/CoinTossLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinTossLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int RandomNumber = 1;
+            int UserGuess = -1;
+
+            while (UserGuess != RandomNumber)
+
+            {
+                RandomNumber = rand.Next(0, 2);
+                Console.WriteLine(RandomNumber);
+
+                Console.WriteLine("Enter heads or Tails)";
+                string UserGuessAsString = Console.ReadLine();
+                
+                const int Heads_Value = 0;
+                const int Tails_Value = 1;
+
+                if (UserGuessAsString == "Heads");
+                else if (UserGuessAsString == "Tails");
+                else
+
+            }
+            
+            
+
+        }
+    }
+}
